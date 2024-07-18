b657d64dafee9c09d43921e9c57adfc69bff5cb6
DESKTOP-QC78LBU\4M-FE <gasina85@gmail.com>
2024-07-18 18:02:01 +0900

    [javascript] tutorial day1

---
+++ prompt/prompt.js
@@ -0,0 +1,7 @@
+let age = prompt('나이를 입력해주세요', 0);
+
+alert(`당신의 나이는 ${age}입니다.`);
+
+let name = prompt('당신의 이름을 입력해주세요');
+
+alert(`안녕하세요 ${name}님`);
---
+++ variable/variable.js
@@ -0,0 +1,16 @@
+let admin, name;
+
+name = "John";
+admin = name;
+
+console.log(admin);
+
+
+
+const birthday = '18.04.1982';
+
+const age = someCode(birthday);
+
+const BIRTHDAY = '18.04.1982'; // 대문자 상수로 바꿔도 괜찮을까요?
+
+const AGE = someCode(BIRTHDAY); // 대문자 상수로 바꿔도 괜찮을까요?
